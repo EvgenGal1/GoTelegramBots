58735b8670a698531ba8ae17d6d9b41ec9c58c50
EvGen Gal <genwolk1@gmail.com>
2024-04-02 04:24:41 +0300

    +обработчики,нов.лог.,кнп.,клвш.,кмд.

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	// telego
 	"github.com/mymmrac/telego"
-	// алиас > обработчик + предикаты(вместо путей)
-	// th "github.com/mymmrac/telego/telegohandler"
-	// алиас >  утилит
+	// алиас th > обработчик + предикаты(вместо путей)
+	th "github.com/mymmrac/telego/telegohandler"
+	// алиас tu > утилит
 	tu "github.com/mymmrac/telego/telegoutil"
 )
 
@@ -27,57 +27,106 @@ func main() {
 	// канал получения обновлений. Созд.executor(`исполнитель`). Есть 2 типа - Long Poling(`Длинный опрос`) | Webhook(`сеть-перехватчик`). Здесь 1 ч/з `обновления ч/з длительный опрос`
 	updates, _ := bot.UpdatesViaLongPolling(nil)
 
+	// обраб.бота c указ.получ.обновления
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	// прекратить обработку обновлений
+	defer bh.Stop()
+
 	// прекратить получать обновления. В стек + fn StopLongPolling(`прекратить долгий опрос`). Вызов при возврате из main по LIFO(от последн.к первому)
 	defer bot.StopLongPolling()
 
 	// цикл получ.обнов.серверов Телеграма
-	for updates := range updates {
-		// е/и не пустое смс
-		if updates.Message != nil {
-			// ID смс
-			chatID := tu.ID(updates.Message.Chat.ID)
-			// клавиатура
-			keyboard := tu.Keyboard(
-				tu.KeyboardRow(
-					tu.KeyboardButton("Старт"),
-					tu.KeyboardButton("Помощь"),
-					tu.KeyboardButton("Тех. поддержка"),
-				),
-				tu.KeyboardRow(
-					tu.KeyboardButton("Отправить локацию").WithRequestLocation(),
-					tu.KeyboardButton("Отправить контакт").WithRequestContact(),
-					tu.KeyboardButton("Отмена"),
-				),
-			)
+	// for updates := range updates {
+	// е/и не пустое смс
+	// if updates.Message != nil {
+	// переезд в обработчик. updates <> update
+	// ID смс, клавиатура, прикреп.клвт.к ответ.смс, отправка клавиатуры, ответ стикером
+	// }
+	// }
 
-			// прикреп.клвт.к ответ.смс ч/з WithReplyMarkup
-			message := tu.Message(
+	// регистр.нов.обраб.по КМД."/start". Узак.fn + тип.обраб.
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		// обработчик
+		// ID смс
+		chatID := tu.ID(update.Message.Chat.ID)
+		// клавиатура
+		keyboard := tu.Keyboard(
+			tu.KeyboardRow(
+				tu.KeyboardButton("Старт"),
+				tu.KeyboardButton("Помощь"),
+				tu.KeyboardButton("Тех. поддержка"),
+			),
+			tu.KeyboardRow(
+				tu.KeyboardButton("Отправить локацию").WithRequestLocation(),
+				tu.KeyboardButton("Отправить контакт").WithRequestContact(),
+				tu.KeyboardButton("Отмена"),
+			),
+		)
+
+		// копир/отправка такого же смс
+		_, _ = bot.CopyMessage(
+			tu.CopyMessage(
 				chatID,
-				"С смс придёт клава. ! но не пришла",
-			).WithReplyMarkup(keyboard)
+				chatID,
+				update.Message.MessageID,
+			),
+		)
 
-			// отправка клавиатуры
-			_, _ = bot.SendMessage(message)
+		// ответ стикером
+		_, _ = bot.SendSticker(
+			// мтд.Sticker(helper из telego) созд.парам.
+			tu.Sticker(
+				// куда отправ.стик.
+				chatID,
+				// мтд.FileFromID указ. ID стикер
+				tu.FileFromID("CAACAgIAAxkBAAEL0-tmCu0FAkLYtjHbWr0vhKSCkxRbiQACpwcAAmMr4gmVkahaRG9rAjQE"),
+			),
+		)
 
-			// копир/отправка такого же смс
-			_, _ = bot.CopyMessage(
-				tu.CopyMessage(
-					chatID,
-					chatID,
-					updates.Message.MessageID,
-				),
-			)
+		// прикреп.клвт.к ответ.смс ч/з WithReplyMarkup
+		message := tu.Message(
+			chatID,
+			"Выбирете пункт меню",
+		).WithReplyMarkup(keyboard)
 
-			// ответ стикером
-			_, _ = bot.SendSticker(
-				// мтд.Sticker(helper из telego) созд.парам.
-				tu.Sticker(
-					// куда отправ.стик.
-					chatID,
-					// мтд.FileFromID указ. ID стикер
-					tu.FileFromID("CAACAgIAAxkBAAEL0-tmCu0FAkLYtjHbWr0vhKSCkxRbiQACpwcAAmMr4gmVkahaRG9rAjQE"),
-				),
-			)
+		// отправка клвт.
+		_, _ = bot.SendMessage(message)
+	}, th.CommandEqual("start"))
+
+	// регистр.нов.обраб.для всех КМД.кроме `/start`
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		// Отправить сообщение
+		_, _ = bot.SendMessage(tu.Message(
+			tu.ID(update.Message.Chat.ID),
+			"Неизвестная команда, используйте /start",
+		))
+	}, th.AnyCommand())
+
+	// регистр.нов.обраб.КНП. Пока > Тех.поддержка и Помощь
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		// Проверяем, что полученное сообщение содержит кнопку "Тех. поддержка"
+		if update.Message != nil {
+			// ID чата, куда отправлять ответ
+			chatID := tu.ID(update.Message.Chat.ID)
+			// перем.хран.смс
+			var smsCre string
+			// общ.улов.и проверка
+			if update.Message.Text == "Тех. поддержка" {
+				// смс > кнп."Тех. поддержка"
+				smsCre = "Вы нажали на кнопку 'Тех. поддержка'"
+			}
+			if update.Message.Text == "Помощь" {
+				smsCre = "Какая вам нужна 'Помощь'"
+			}
+			// отправ.смс от кажд.кнп.
+			_, _ = bot.SendMessage(tu.Message(
+				chatID,
+				smsCre,
+			))
 		}
-	}
+	}, th.AnyMessage())
+
+	// старт.обраб.обновлений
+	bh.Start()
 }
